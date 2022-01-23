26ed317bc7e9d789431492c173661f3b574ad58c
Jehad Abusir <jabusir@gmail.com>
2022-01-23 11:46:10 -0800

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect } from "react";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const [deck, setDeck] = useState("");
+  const [cardCount, setCardCount] = useState(0);
+  const [hand, setHand] = useState({});
+  const [fullHouse, setFullHouse] = useState(false);
+
+  return <div className="App"></div>;
 }
 
 export default App;
